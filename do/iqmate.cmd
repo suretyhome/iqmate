93534082c13f4cf54a87e42b3346f83bc440e7e2
Evan Gibson <155255244+egibson940@users.noreply.github.com>
2025-06-05 14:19:13 -0500

    Update iqmate.cmd for !var! variable calls

--- do/iqmate.cmd
+++ do/iqmate.cmd
@@ -1,4 +1,4 @@
-::Turn off console printing and enabledelayedexansion (change variables within for loops)
+:: Turn off console printing and enable delayed expansion (change variables within for loops)
 @echo off
 setlocal enabledelayedexpansion
 
@@ -24,27 +24,27 @@ for %%A in (%*) do (
 )
 
 :: Check if arguments (Host IP and Secure Token in that order) were found. If not => usage
-if "%arg1%"=="" GOTO :usage
-if "%arg2%"=="" GOTO :usage
+if "!arg1!"=="" GOTO :usage
+if "!arg2!"=="" GOTO :usage
 
 :: Check if shared directory exists and make one if not
-if not exist "%shared_dir%" (
-    mkdir "%shared_dir%"
+if not exist "!shared_dir!" (
+    mkdir "!shared_dir!"
 )
 
 :: Reset directory if requested
-if "%should_reset%"=="1" (
-    set /P "confirm=Are you sure you want to delete everything in %shared_dir%? (y/n): "
-    if /I "%confirm%" == "y" (
-        del /Q "%shared_dir%\*"
+if "!should_reset!"=="1" (
+    set /P "confirm=Are you sure you want to delete everything in !shared_dir!? (y/n): "
+    if /I "!confirm!" == "y" (
+        del /Q "!shared_dir!\*"
     )
 )
 
 :: Run Docker command with saved arguments
 docker run --rm -it ^
     -p 1880:1880 -p 1883:1883 ^
-    -v "%shared_dir%":/iqmate -w /iqmate ^
-    -e IQPANEL_HOST=%arg1% -e IQPANEL_TOKEN=%arg2% ^
+    -v "!shared_dir!":/iqmate -w /iqmate ^
+    -e IQPANEL_HOST=!arg1! -e IQPANEL_TOKEN=!arg2! ^
     suretyhome/iqmate /bin/bash
 
 GOTO :eof
@@ -54,7 +54,7 @@ GOTO :eof
     echo Usage: [--shared=Path] [--reset] ^<IQ Panel Host^> ^<IQ Panel Secure Token^>
     echo.    IQ Panel Host: The hostname or IP address of the IQ Panel
     echo.    IQ Panel Secure Token: The secure token from the IQ Panel Control4 integration
-    echo.    --shared: Optionally specify path the shared directory (Defaults to .\shared)
+    echo.    --shared: Optionally specify path to the shared directory (Defaults to .\shared)
     echo.    --reset: Optionally delete contents of shared directory and start from scratch
     echo Visit https://support.suretyhome.com/t/surety-iq-mate/32721 for documentation.
     exit /b 1
