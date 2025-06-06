f9e170182b6bd98d449f250c2fce4c293c69125e
Ryan Boder <ryanboder@tuta.com>
2025-06-05 21:11:14 -0500

    Rename --shared to --workspace

--- do/iqmate.cmd
+++ do/iqmate.cmd
@@ -2,28 +2,28 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Default shared directory and initialize variables for arguments passed
-set "shared_dir=%CD%\shared"
+:: Default workspace directory and initialize variables for arguments passed
+set "workspace=%CD%\workspace"
 set "should_reset="
 set "arg1="
 set "arg2="
 
 :: Parse arguments and assign variables
-set "next_is_shared_value="
+set "next_is_workspace_value="
 for %%A in (%*) do (
     set "current_arg=%%A"
     
-    if "!next_is_shared_value!" == "1" (
-        set "shared_dir=!current_arg!"
-        set "next_is_shared_value="
+    if "!next_is_workspace_value!" == "1" (
+        set "workspace=!current_arg!"
+        set "next_is_workspace_value="
     ) else (
-        :: Check for --shared= with equals
-        echo !current_arg! | find "--shared=" >nul
+        :: Check for --workspace= with equals
+        echo !current_arg! | find "--workspace=" >nul
         if !errorlevel! == 0 (
             set "temp_arg=!current_arg!"
-            set "shared_dir=!temp_arg:*--shared=!"
-        ) else if "!current_arg!" == "--shared" (
-            set "next_is_shared_value=1"
+            set "workspace=!temp_arg:*--workspace=!"
+        ) else if "!current_arg!" == "--workspace" (
+            set "next_is_workspace_value=1"
         ) else if "!current_arg!" == "--reset" (
             set "should_reset=1"
         ) else (
@@ -40,28 +40,28 @@ for %%A in (%*) do (
 if "!arg1!"=="" GOTO :usage
 if "!arg2!"=="" GOTO :usage
 
-:: Check if shared directory exists and make one if not
-if not exist "!shared_dir!" (
-    mkdir "!shared_dir!"
+:: Check if workspace directory exists and make one if not
+if not exist "!workspace!" (
+    mkdir "!workspace!"
 )
 
 :: Reset directory if requested
 if "!should_reset!"=="1" (
-    set /P "confirm=Are you sure you want to delete everything in !shared_dir!? (y/n): "
+    set /P "confirm=Are you sure you want to delete everything in !workspace!? (y/n): "
     if /I "!confirm!" == "y" (
-        del /Q "!shared_dir!\*"
+        del /Q "!workspace!\*"
     )
 )
 
-::Get absolute path from shared_dir
-if not "!shared_dir:~1,1!" == ":" (
-    set "shared_dir=%CD%\!shared_dir!"
+::Get absolute path from workspace
+if not "!workspace:~1,1!" == ":" (
+    set "workspace=%CD%\!workspace!"
 )
 
 :: Run Docker command with saved arguments
 docker run --rm -it ^
     -p 1880:1880 -p 1883:1883 ^
-    -v "!shared_dir!":/iqmate -w /iqmate ^
+    -v "!workspace!":/iqmate -w /iqmate ^
     -e IQPANEL_HOST=!arg1! -e IQPANEL_TOKEN=!arg2! ^
     suretyhome/iqmate /bin/bash
 
@@ -69,10 +69,10 @@ GOTO :eof
 
 :: What is displayed if arguments are incorrect
 :usage
-    echo Usage: [--shared=Path] [--reset] ^<IQ Panel Host^> ^<IQ Panel Secure Token^>
+    echo Usage: [--workspace=Path] [--reset] ^<IQ Panel Host^> ^<IQ Panel Secure Token^>
     echo.    IQ Panel Host: The hostname or IP address of the IQ Panel
     echo.    IQ Panel Secure Token: The secure token from the IQ Panel Control4 integration
-    echo.    --shared: Optionally specify path to the shared directory (Defaults to .\shared)
-    echo.    --reset: Optionally delete contents of shared directory and start from scratch
+    echo.    --workspace: Optionally specify path to the workspace folder (Defaults to .\workspace)
+    echo.    --reset: Optionally delete contents of the workspace and start from scratch
     echo Visit https://support.suretyhome.com/t/surety-iq-mate/32721 for documentation.
     exit /b 1
--- do/iqmate.sh
+++ do/iqmate.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ $# -lt 2 ]]; then
-    echo 'Usage: [--shared=Path] [--reset] <IQ Panel Host> <IQ Panel Secure Token>'
+    echo 'Usage: [--workspace=Path] [--reset] <IQ Panel Host> <IQ Panel Secure Token>'
     echo '    IQ Panel Host: The hostname or IP address of the IQ Panel'
     echo '    IQ Panel Secure Token: The secure token from the IQ Panel Control4 integration'
-    echo '    --shared: Optionally specify path the shared directory (Defaults to ./shared)'
-    echo '    --reset: Optionally delete contents of shared directory and start from scratch'
+    echo '    --workspace: Optionally specify path the workspace directory (Defaults to ./workspace)'
+    echo '    --reset: Optionally delete contents of the workspace and start from scratch'
     echo 'Visit https://support.suretyhome.com/t/surety-iq-mate/32721 for documentation.'
     exit 1
 fi
@@ -15,31 +15,31 @@ args=()
 for arg; do
     case $arg in 
         --reset) should_reset=1 ;;
-        --shared=*) shared_dir="${arg:9}" ; shared_dir="${shared_dir/#\~/$HOME}" ;;
+        --workspace=*) workspace="${arg:12}" ; workspace="${workspace/#\~/$HOME}" ;;
         *) args+=($arg) ;;
     esac
 done
 
-# Figure out what the shared directory will be and create if necessary
-if [[ -z "$shared_dir" ]]; then
-    shared_dir="$PWD/shared"
+# Figure out what the workspace directory will be and create if necessary
+if [[ -z "$workspace" ]]; then
+    workspace="$PWD/workspace"
 fi
-if [[ ! -d "$shared_dir" ]]; then
-    mkdir "$shared_dir"
+if [[ ! -d "$workspace" ]]; then
+    mkdir "$workspace"
 fi
 
 # Reset if needed
 if [[ -n "$should_reset" ]]; then
-    read -p "Are you sure you want to delete everything in $shared_dir? " -n 1 -r
+    read -p "Are you sure you want to delete everything in $workspace? " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        rm -Rf "$shared_dir"/*
+        rm -Rf "$workspace"/*
     fi
 fi
 
 # Run IQ Mate
 docker run --rm -it \
     -p 1880:1880 -p 1883:1883 \
-    -v "$shared_dir":/iqmate -w /iqmate \
+    -v "$workspace":/iqmate -w /iqmate \
     -e IQPANEL_HOST=${args[0]} -e IQPANEL_TOKEN=${args[1]} \
     suretyhome/iqmate /bin/bash
